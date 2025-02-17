671cbc3534d01542665e450b62c62a8c87d693d2
Sreejish <sreejishm92@gmail.com>
2025-02-17 12:13:49 +0530

    new view is added

--- newstudyapp/urls.py
+++ newstudyapp/urls.py
@@ -1,3 +1,4 @@
 from django.urls import path
 from.import  views
-urlpatterns=[path("",views.home,name="home")]
+urlpatterns=[path("",views.home,name="home"),
+             path("path1",views.about,name="about")]
