4b4eb03f114bad094eff6d02ce286ec057320068
Sreejish <sreejishm92@gmail.com>
2025-02-17 12:36:34 +0530

    contact view is created

--- newstudyapp/urls.py
+++ newstudyapp/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
 from.import  views
 urlpatterns=[path("",views.home,name="home"),
-             path("path1",views.about,name="about")]
+             path("path1",views.about,name="about"),
+             path("path2",views.contact,name="contact")]
